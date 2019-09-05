b540a3a015c0e8287f06d820de293556a4d6842f
Dennis Padiernos <depadiernos@gmail.com>
2019-09-04 21:03:49 -0500

    finish last stretch goal

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span key={movie.id} className="saved-movie">{movie.title}</span>
+      <NavLink to={`/movie/${movie.id}`} key={movie.id} activeClassName="saved-active" className="saved-movie">{movie.title}</NavLink>
     ))}
     <Link to="/" className="home-button">Home</Link>
   </div>
