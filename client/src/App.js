5cc4fe5b100c3c35be537b37a1cebe86ca84fa3a
Dennis Padiernos <depadiernos@gmail.com>
2019-09-04 20:59:14 -0500

    implement stretch goals.

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,16 @@ import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
+    console.log(savedList)
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <Route path="/" exact component={MoveList}/>
-      <Route path="/movie/:id" component={Movie}/>
+      <Route path="/" exact component={MoveList} />
+      <Route path="/movie/:id" render={(props) => <Movie {...props} addToSavedList={addToSavedList} />} />
     </div>
   );
 };
