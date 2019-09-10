f8fd4895e78306f94dbfe5cc6e26305bebdfb37e
Dennis Padiernos <depadiernos@gmail.com>
2019-09-10 18:21:30 -0500

    update project to fix duplicate saved movies and a catch-all redirect.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom'
-import MoveList from './Movies/MovieList'
-import Movie from './Movies/Movie'
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react"
+import { Route, Redirect } from "react-router-dom"
+import MovieList from "./Movies/MovieList"
+import Movie from "./Movies/Movie"
+import SavedList from "./Movies/SavedList"
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const addToSavedList = movie => {
-    (!savedList.includes(movie)) && setSavedList([...savedList, movie]);
+  const [savedList, setSavedList] = useState([])
+  const addToSavedList = (movie) => {
+    !savedList.find((savedMovie) => savedMovie.title === movie.title) &&
+      setSavedList([...savedList, movie])
     console.log(savedList)
-  };
+  }
 
   return (
     <div>
       <SavedList list={savedList} />
-      <Route path="/" exact component={MoveList} />
-      <Route path="/movie/:id" render={(props) => <Movie {...props} addToSavedList={addToSavedList} />} />
+      <Route path="/" exact component={MovieList} />
+      <Route
+        path="/movie/:id"
+        render={(props) => <Movie {...props} addToSavedList={addToSavedList} />}
+      />
+      <Redirect to="/" />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
